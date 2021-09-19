7f8df87ec8768a63d17b9c702c93253c9df7755d
Javier Jiménez Castillo <javier.jimenez@iespoligonosur.org>
2021-09-19 13:14:39 +0200

    Commit inicial

---
+++ Geometria/src/geometria/Cuadrado.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometria;
+
+/**
+ *
+ * @author javier
+ */
+public class Cuadrado implements FiguraGeometrica
+{
+    private double lado;
+    private double areaCuadrado;
+
+    public Cuadrado(double lado) 
+    {
+        this.lado = lado;
+    }
+    
+    public double getLado() 
+    {
+        return lado;
+    }
+
+    public void setLado(double lado) 
+    {
+        this.lado = lado;
+    }
+
+    public double getareaCuadrado() 
+    {
+        return areaCuadrado;
+    }    
+    
+    @Override
+    public void area() 
+    {
+        areaCuadrado = lado * lado;
+    }    
+}
---
+++ Geometria/src/geometria/FiguraGeometrica.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometria;
+
+/**
+ *
+ * @author javier
+ */
+public interface FiguraGeometrica 
+{
+    void area();    
+}
